a4e0549cca5806643d36824542ebd46382dd2bd3
Yann Bouchereau <yann.bchr@gmail.com>
2024-11-15 15:29:56 -0930

    initial commit

---
+++ backend/src/main/java/com/yannkb/timezone_app/TimezoneAppApplication.java
@@ -0,0 +1,13 @@
+package com.yannkb.timezone_app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimezoneAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TimezoneAppApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/yannkb/timezone_app/TimezoneAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.yannkb.timezone_app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimezoneAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

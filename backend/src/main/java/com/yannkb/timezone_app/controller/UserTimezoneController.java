86c914691986183a3d0e5791a42e9b750bdba2c8
Yann Bouchereau <yann.bchr@gmail.com>
2024-11-21 18:08:22 -0930

    feat: add convert function

--- backend/src/main/java/com/yannkb/timezone_app/controller/UserTimezoneController.java
+++ backend/src/main/java/com/yannkb/timezone_app/controller/UserTimezoneController.java
@@ -1,10 +1,17 @@
 package com.yannkb.timezone_app.controller;
 
+import com.yannkb.timezone_app.entity.TimeConversionRequest;
+import com.yannkb.timezone_app.entity.TimezoneRequest;
 import com.yannkb.timezone_app.entity.UserTimezone;
 import com.yannkb.timezone_app.service.UserTimezoneService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/usertimezones")
@@ -12,9 +19,14 @@ public class UserTimezoneController {
     @Autowired
     private UserTimezoneService userTimezoneService;
 
+    @GetMapping("/available")
+    public List<String> getAvailableTimezones() {
+        return userTimezoneService.getAllAvailableTimezones();
+    }
+
     @PostMapping
-    public UserTimezone createUserTimezone(@RequestBody UserTimezone userTimezone) {
-        return userTimezoneService.save(userTimezone);
+    public UserTimezone createUserTimezone(@RequestBody TimezoneRequest request) {
+        return userTimezoneService.save(request.getLabel(), request.getZoneId());
     }
 
     @GetMapping("/{id}")
@@ -37,5 +49,19 @@ public class UserTimezoneController {
     public ResponseEntity<Void> deleteUserTimezone(@PathVariable Long id) {
         userTimezoneService.deleteUserTimezone(id);
         return ResponseEntity.ok().build();
+    }
+
+    @PostMapping("/convert")
+    public Map<String, String> convertTime(@RequestBody TimeConversionRequest request) {
+        ZonedDateTime converted = userTimezoneService.convertTime(
+                request.getSourceZoneId(),
+                request.getDateTime(),
+                request.getTargetZoneId()
+        );
+
+        return Map.of(
+                "convertedTime", converted.format(DateTimeFormatter.ISO_LOCAL_DATE_TIME),
+                "offset", converted.getOffset().getId()
+        );
     }
 }
